bfae5ea797dc836f8f68d8350c1e93f8e14cbe54
Sumit Sah <ssah48731@gmail.com>
2024-02-02 13:11:19 +0545

    updated-search-button

--- sidebar.js
+++ sidebar.js
@@ -5,43 +5,53 @@ const bodyElement = document.querySelector('.body');
   modeSwitch = document.querySelector('.mode'),
   modeTextElement = document.querySelector('.mode-text');
 
-  let isFullNameDisplaying = true;
+function toggleSidebar() {
+  sidebarElement.classList.toggle('close');
+    toggleElement.classList.toggle('bx-chevron-right');
 
-  toggleElement.addEventListener(
-    'click',
-    () => {
-      sidebarElement.classList.toggle('close');
-
-      if(document.getElementById('heading').innerText === 'Navbar')
-        {
-          document.getElementById('heading').innerText = 'Nav';
-        } else {
-          document.getElementById('heading').innerText = 'Navbar';
-        }
-
-        if(document.getElementById('name').innerText === `©Sumit Sah`) {
-          document.getElementById('name').innerHTML = `&#169;SS`;
-        } else {
-          document.getElementById('name').innerHTML = `&#169;Sumit Sah`;
-        }
-
-    }
-
-  )
-
-  modeSwitch.addEventListener(
-    'click',
-    () => {
-      bodyElement.classList.toggle('dark');
-
-      if(bodyElement.classList.contains('dark')) {
-        modeTextElement.innerHTML = 'Light Mode';
-        document.querySelector('.sun').style.opacity = 1;
-        document.querySelector('.moon').style.opacity = 0;
+    if(document.getElementById('heading').innerText === 'Navbar')
+      {
+        document.getElementById('heading').innerText = 'Nav';
       } else {
-        modeTextElement.innerHTML = 'Dark Mode';
-        document.querySelector('.sun').style.opacity = 0;
-        document.querySelector('.moon').style.opacity = 1;
+        document.getElementById('heading').innerText = 'Navbar';
       }
+
+      if(document.getElementById('name').innerText === `©Sumit Sah`) {
+        document.getElementById('name').innerHTML = `&#169;SS`;
+      } else {
+        document.getElementById('name').innerHTML = `&#169;Sumit Sah`;
+      }
+}
+
+toggleElement.addEventListener(
+  'click',
+  () => {
+    toggleSidebar();
     }
-  )
+)
+
+SearchBtnElement.addEventListener(
+  'click',
+  () => {
+    if(sidebarElement.classList.contains('close')) {
+      toggleSidebar();
+    }
+  }
+)
+
+modeSwitch.addEventListener(
+  'click',
+  () => {
+    bodyElement.classList.toggle('dark');
+
+    if(bodyElement.classList.contains('dark')) {
+      modeTextElement.innerHTML = 'Light Mode';
+      document.querySelector('.sun').style.opacity = 1;
+      document.querySelector('.moon').style.opacity = 0;
+    } else {
+      modeTextElement.innerHTML = 'Dark Mode';
+      document.querySelector('.sun').style.opacity = 0;
+      document.querySelector('.moon').style.opacity = 1;
+    }
+  }
+)
